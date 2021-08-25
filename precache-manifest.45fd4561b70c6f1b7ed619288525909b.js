1d2270d33e0722ba248ff268c9fba5c76102c066
VeronikaLushchik <loaaary212@gmail.com>
2021-08-25 16:02:18 +0300

    Updates

---
+++ precache-manifest.45fd4561b70c6f1b7ed619288525909b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3d5a0d7004c9483bc586da41644342",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea426b14862d361606cc",
+    "url": "./static/css/main.dd1d1171.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea426b14862d361606cc",
+    "url": "./static/js/main.b4e752a5.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
